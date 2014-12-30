c698425e84535d632409dae711b75721dd82f81d
Mark O Grady <markogrady.code@gmail.com>
2014-12-30 21:18:27 +0000

    Update Blog.php

--- app/views/Blog.php
+++ app/views/Blog.php
@@ -4,7 +4,18 @@ use contracts\Rendable;
 
 class Blog implements Rendable
 {
+   /**
+    * Path for the blog page
+    * 
+    * @var string $view
+    */
     public $view;
+    
+   /**
+    * Data retrieved from the database concerning the blog page
+    * 
+    * @var array $data
+    */
     public $data;
 
     public function __construct($view, $data)
