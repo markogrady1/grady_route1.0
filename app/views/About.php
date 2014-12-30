89315598591348fed03752932839f2f7cbdfe955
Mark O Grady <markogrady.code@gmail.com>
2014-12-30 21:16:46 +0000

    Fix docblocks

--- app/views/About.php
+++ app/views/About.php
@@ -4,7 +4,18 @@ use contracts\Rendable;
 
 class About implements Rendable
 {
+    /**
+     * Path for the about page
+     * 
+     * @var string $data
+     */
     public $view;
+    
+    /**
+     * Data retrieved from the database concerning the about page
+     * 
+     * @var array $data
+     */
     public $data;
 
     public function __construct($view, $data)
@@ -13,6 +24,11 @@ class About implements Rendable
         $this->data = $data;
     }
 
+    /**
+     * Require a given view
+     * 
+     * @return void
+     */
     public function render()
     {
         require $this->view;
