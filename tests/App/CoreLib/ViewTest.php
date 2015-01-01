f7b992f8715a785175bba8119338167a5261f24f
Mark O Grady <markogrady.code@gmail.com>
2015-01-01 13:55:44 +0000

    add docBlock to ViewTest.php

--- tests/App/CoreLib/ViewTest.php
+++ tests/App/CoreLib/ViewTest.php
@@ -3,6 +3,11 @@
 
 class ViewTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * Instance of the View class
+     * 
+     * @var coreLib\View $view
+     */
     public $view;
 
     public function setUp()
@@ -10,6 +15,11 @@ class ViewTest extends \PHPUnit_Framework_TestCase
         $this->view = new View();
     }
 
+    /**
+     * Test to ensure a boolean value of false is returned when a view does not exist
+     * 
+     * @return void
+     */
     public function testResolveViewReturnsFalseWhenClassDoesNotExist()
     {
         $this->assertFalse(view::resolveView('somePath/someWhere/someFile.php', 'SomeClass', array('foo' => 'bar')));
